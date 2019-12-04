b207051fee71d3ade97bdd863a174bd1848ab011
D Demidov <dmitr.demidov2018@yandex.ru>
2019-12-04 14:16:07 +0300

    First EPAM comit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+        System.out.println("Hello World!");
+        int i = 1;
+        System.out.println(i);
+        i = i+1;
+        System.out.println(i);
+        i = i+1;
+        System.out.println(i);
+
+        int a = 1;
+        int b = 2;
+        int c = a+b;
+        System.out.println("sum = "+c);
+
+        int a2 = 1;
+        int b2 = 2;
+        System.out.println("a>b = "+ (a>b));
+        System.out.println("a<b = "+ (a<b));
+
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello+ " " +world+ "!");
+
+    }
+}
